6151c05b307ab7460cdc37bbaef5f16b64f0b6ea
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-06-21 15:41:20 -0300

    Requisitos 2, 3 e 4. Implementa inputs email e senha com testId.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 function Login() {
-  return (<p>Login</p>);
+  return (
+    <main>
+      <header>Login</header>
+      <div>
+        <input
+          type="email"
+          name="email"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+          // onChange={ () = onHandleChange()}
+        />
+        <button
+          data-testid="login-submit-btn"
+          type="button"
+        >
+          Enter
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default Login;
