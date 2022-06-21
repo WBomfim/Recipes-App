935250f26eaa1bc0bb60b5a800ef983363d2c014
BrenoBTrindade <brenokaminski@hotmail.com>
2022-06-21 14:32:45 -0300

    [feat]: Header e HeaderWithoutSearch

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
 
 function Explore() {
   return (
     <div>
+      <HeaderWithoutSearch title="Explore" />
       <Footer />
     </div>
   );
