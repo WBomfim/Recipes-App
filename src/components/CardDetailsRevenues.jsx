53f07e1f9b03593d4319aab29908744996c13e02
WBomfim <willianbomfim@hotmail.com>
2022-06-21 21:42:58 -0300

    Refactor: Removido o arquivo App.test

--- src/components/CardDetailsRevenues.jsx
+++ src/components/CardDetailsRevenues.jsx
@@ -4,14 +4,12 @@ import React from 'react';
 function CardDetailsRevenues() {
   return (
     <div>
-      <p data-testid="recipe-category">{ category }</p>
       <h2>Ingredients</h2>
       {/* <p data-testid="`${index}-ingredient-name-and-measure`">{ ingredients }</p> */}
       <h2>Instructions</h2>
       <p data-testid="instructions">{ instructions }</p>
       {/* <video src={ video } data-testid="video" />
       <CardRevenues data-testid="`${index}-recomendation-card`" /> */}
-      <button data-testid="start-recipe-btn" type="button">Start Recipe</button>
     </div>
   );
 }
