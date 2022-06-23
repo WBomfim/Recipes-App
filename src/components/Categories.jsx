4a986b839ff64f8256677e1927908bdc3377f7b9
BrenoBTrindade <brenokaminski@hotmail.com>
2022-06-22 21:44:23 -0300

    [feat]: requisito 31 concluido

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -14,17 +14,20 @@ function Categories() {
   const five = 5;
   const location = useLocation().pathname.split('/')[1];
 
+  const getAllRecipes = async () => {
+    if (location === 'foods') {
+      const foods = await getFoods();
+      setExibitionRevenues(foods);
+    }
+
+    if (location === 'drinks') {
+      const drinks = await getDrinks();
+      setExibitionRevenues(drinks);
+    }
+  };
   const onClickButton = async (categoryName) => {
     if (categorySelect.category === categoryName[0]) {
-      if (location === 'foods') {
-        const foods = await getFoods();
-        setExibitionRevenues(foods);
-      }
-
-      if (location === 'drinks') {
-        const drinks = await getDrinks();
-        setExibitionRevenues(drinks);
-      }
+      await getAllRecipes();
     } else {
       setCategorySelect({ type: location, category: categoryName[0] });
     }
@@ -32,6 +35,14 @@ function Categories() {
 
   return (
     <section>
+
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={ () => getAllRecipes() }
+      >
+        All
+      </button>
       { categories.map((category) => Object.values(category))
         .slice(0, five)
         .map((categoryName) => (
