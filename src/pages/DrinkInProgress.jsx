b4a3a57b0046cd400a8bf62b42c62a24ace243ac
WBomfim <willianbomfim@hotmail.com>
2022-06-23 02:19:13 -0300

    Fix: Corrigindo conflito no PR

--- src/pages/DrinkInProgress.jsx
+++ src/pages/DrinkInProgress.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import RevenuesContext from '../context/RevenuesContext';
 /* import HeaderRevenue from '../components/HeaderRevenue';
 import ShowDetailsProcess from '../components/ShowDetailsProcess'; */
@@ -7,7 +7,7 @@ import Button from '../components/Button';
 
 function DrinkInProgress() {
   const { getDataById } = useContext(RevenuesContext);
-  const id = useLocation().pathname.replace(/\D/g, '');
+  /* const id = useLocation().pathname.replace(/\D/g, ' '); */
 
   useEffect(() => {
     getDataById('drinks', '17005');
