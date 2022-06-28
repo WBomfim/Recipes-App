cd3304985957dda1a8efa57ddb180f3f29890953
WBomfim <willianbomfim@hotmail.com>
2022-06-28 17:17:41 -0300

    Feat: Criado os filtros por tipo de receitas finalizadas.

--- src/components/FilterBarStorage.jsx
+++ src/components/FilterBarStorage.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RevenuesContext from '../context/RevenuesContext';
+import { getDoneRecipes } from '../helpers/storageDoneRecipes';
 
 function FilterBarStorage() {
+  const { setExibitionRevenues } = useContext(RevenuesContext);
+
+  const filterAll = () => {
+    const recipes = getDoneRecipes();
+    setExibitionRevenues([...recipes]);
+  };
+
+  const filterFood = () => {
+    const doneRecipes = getDoneRecipes();
+    const filterRevenuesType = doneRecipes.filter((recipe) => recipe.type === 'food');
+    setExibitionRevenues([...filterRevenuesType]);
+  };
+
+  const filterDrink = () => {
+    const doneRecipes = getDoneRecipes();
+    const filterRevenuesType = doneRecipes.filter((recipe) => recipe.type === 'drink');
+    setExibitionRevenues([...filterRevenuesType]);
+  };
+
   return (
     <div>
       <button
         type="button"
         data-testid="filter-by-all-btn"
+        onClick={ filterAll }
       >
         All
       </button>
       <button
         type="button"
         data-testid="filter-by-food-btn"
+        onClick={ filterFood }
       >
         Food
       </button>
       <button
         type="button"
         data-testid="filter-by-drink-btn"
+        onClick={ filterDrink }
       >
         Drink
       </button>
